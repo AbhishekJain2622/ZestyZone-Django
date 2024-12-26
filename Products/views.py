2fa1deb0a8b2ecd437d2ea281e4bb319ed9864d2
Abhishek Jain <121627114+AbhishekJain2622@users.noreply.github.com>
2024-12-26 03:33:37 -0800

    initial commit

--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from .models import *
+from math import ceil
 
 # Create your views here.
 def Home(request):
@@ -9,9 +10,37 @@ def Home(request):
 def About(request):
     return render(request,'about.html')
 
-def menu(request):
-    return render(request,'menu.html')
+from math import ceil
+from django.shortcuts import render
+from .models import Product
 
+def menu(request):
+    allProds = []
+    catprods = Product.objects.values('category', 'id')  # Fetch distinct categories
+    cats = {item['category'] for item in catprods}       # Create a set of categories
+    
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)      # Fetch all products in this category
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))       # Calculate the number of slides (if needed)
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds': allProds}
+    return render(request, "menu.html", params)
+# def menu(request):
+#     allProds = []
+#     catprods = Product.objects.values('category','id')
+#     print(catprods)
+#     cats = {item['category'] for item in catprods}
+#     for cat in cats:
+#         prod= Product.objects.filter(category=cat)
+#         n=len(prod)
+#         nSlides = n // 4 + ceil((n / 4) - (n // 4))
+#         allProds.append([prod, range(1, nSlides), nSlides])
+
+#     params= {'allProds':allProds}
+
+#     return render(request,"menu.html",params)
 
 def Contact(request):
     if request.method == "POST":

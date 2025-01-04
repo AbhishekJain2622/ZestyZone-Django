5f09d2f5eeb4ffa8dd633085ae263a4124467736
Abhishek Jain <121627114+AbhishekJain2622@users.noreply.github.com>
2025-01-04 18:48:56 +0530

    abhishek

--- ZestyZone/settings.py
+++ ZestyZone/settings.py
@@ -148,8 +148,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jain49218@gmail.com'
-EMAIL_HOST_PASSWORD = "prlp itqf bidl aqhe"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
 
 
 
